d3f7df0fd08a211c731200c302bc7e7f29a1594e
SanggeunCho <wtr011124@gmail.com>
2026-09-22 10:44:29 +0900

    0036

--- src/main/java/com/back/boundedContext/market/domain/Order.java
+++ src/main/java/com/back/boundedContext/market/domain/Order.java
@@ -21,6 +21,7 @@ public class Order extends BaseIdAndTime {
     @ManyToOne(fetch = FetchType.LAZY)
     private MarketMember buyer;
 
+    private LocalDateTime cancelDate;
     private LocalDateTime requestPaymentDate;
     private LocalDateTime paymentDate;
 
@@ -59,6 +60,14 @@ public class Order extends BaseIdAndTime {
 
     public boolean isPaid() {
         return paymentDate != null;
+    }
+
+    public boolean isCanceled() {
+        return cancelDate != null;
+    }
+
+    public boolean isPaymentInProgress() {
+        return requestPaymentDate != null && paymentDate == null && cancelDate == null;
     }
 
     public void requestPayment(long pgPaymentAmount) {
